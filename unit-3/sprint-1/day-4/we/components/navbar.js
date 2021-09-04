6b6b13b2bb5140e450be5361bef1470fb6b5bbf8
Abhinav <abhinavgujral33@gmail.com>
2021-09-04 15:24:22 +0530

    movies_app

---
+++ unit-3/sprint-1/day-4/we/components/navbar.js
@@ -0,0 +1,17 @@
+function navbar(){
+
+    return ` <div id="navbar">
+    <h3><a href="/">Home</a></h3>
+
+    <div id="options">
+      <h3><a href="./Electronics.html">Electronics</a></h3>
+
+      <h3><a href="./jwellery.html">Jewellery</a></h3>
+      <h3><a href="/">Register</a></h3>
+      <h3><a href="/">Login</a></h3>
+    </div>
+  </div>`
+
+}
+
+export default navbar
