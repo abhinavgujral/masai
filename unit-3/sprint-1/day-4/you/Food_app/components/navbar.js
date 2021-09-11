c98eb13023f96c7b52b15d21fab5bf7d02c6a4c6
Abhinav <abhinavgujral33@gmail.com>
2021-09-11 10:50:56 +0530

    translator_app

---
+++ unit-3/sprint-1/day-4/you/Food_app/components/navbar.js
@@ -0,0 +1,9 @@
+export function navbar(){
+    return `
+    <nav>
+    <h1>FEED HUNGER</h1>
+    <h2 id="nav_home"> <a href="#">Home</a></h2>
+    <h2><a href="#">Recipe of the day</a></h2>
+    <h2><a href="#">Latest recipe</a></h2>
+</nav>`;
+}
