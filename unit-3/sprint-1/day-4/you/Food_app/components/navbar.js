c14d174da52029ab5ef2b5dcdcdab08ae812fccc
Abhinav <abhinavgujral33@gmail.com>
2021-09-11 16:51:07 +0530

    Food_app

--- unit-3/sprint-1/day-4/you/Food_app/components/navbar.js
+++ unit-3/sprint-1/day-4/you/Food_app/components/navbar.js
@@ -2,8 +2,9 @@ export function navbar(){
     return `
     <nav>
     <h1>FEED HUNGER</h1>
-    <h2 id="nav_home"> <a href="#">Home</a></h2>
-    <h2><a href="#">Recipe of the day</a></h2>
-    <h2><a href="#">Latest recipe</a></h2>
+    <h2 id="nav_home"><a href="./index.html">Home</a></h2>
+    <h2><a href="./recipe_of_day.html">Recipe of the day</a></h2>
+    <h2><a href="./latest_recipe.html">Latest recipe</a></h2>
 </nav>`;
 }
+
