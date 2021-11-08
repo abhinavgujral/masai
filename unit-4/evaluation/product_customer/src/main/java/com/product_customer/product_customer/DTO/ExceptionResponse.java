1d4fa67acf72bc852b245931b192bb4edd775a90
Abhinav <abhinavgujral33@gmail.com>
2021-11-08 16:44:17 +0530

    evaluation

--- unit-4/evaluation/product_customer/src/main/java/com/product_customer/product_customer/DTO/ExceptionResponse.java
+++ unit-4/evaluation/product_customer/src/main/java/com/product_customer/product_customer/DTO/ExceptionResponse.java
@@ -1,9 +1,13 @@
 package com.product_customer.product_customer.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExceptionResponse {
     private Date timestamp;
     private String message;
--- unit-4/evaluation/product_customer/src/main/java/com/product_customer/product_customer/Exceptions/NullValue.java
+++ unit-4/evaluation/product_customer/src/main/java/com/product_customer/product_customer/Exceptions/NullValue.java
@@ -1,11 +1,12 @@
 package com.product_customer.product_customer.Exceptions;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class CustomException extends RuntimeException{
-    public CustomException(String message) {
+public class NullValue extends RuntimeException{
+    public NullValue(String message) {
         super(message);
     }
 }
